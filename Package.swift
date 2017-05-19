e134cc9eee53fe985a311b5464077c5ff0b22100
Alexey Fayzullov <alex.f@techery.io>
2017-05-20 00:06:24 +0300

    Add RxSwift dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyJanet"
+    name: "SwiftyJanet",
+    dependencies: [
+        .Package(url: "https://github.com/ReactiveX/RxSwift", majorVersion: 3)
+    ],
+    exclude: [
+        "Tests"
+    ]
 )
